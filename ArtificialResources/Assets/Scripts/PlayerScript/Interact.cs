fcbff9fb78a3b0fa7bcbff50ad4ffc6a015f8a76
Gino Schaap <35615@ma-web.nl>
2025-09-22 11:19:32 +0200

    fixed a bug where turning off the movement makes you jump and tested the scene with the ai and the desktop Render Texture

--- ArtificialResources/Assets/Scripts/PlayerScript/Interact.cs
+++ ArtificialResources/Assets/Scripts/PlayerScript/Interact.cs
@@ -11,29 +11,31 @@ public class Interact : MonoBehaviour
     private Camera cam;
     private PlayerCameraScript pcs;
 
+    [SerializeField]private PlayerMovement _playerMovement;
+    [SerializeField]private PlayerCameraScript _playerCameraScript;
+    [SerializeField]private CharacterController character;
+    [SerializeField]private Rigidbody _rigidbody;
+
     private void Start()
     {
         cam = GetComponentInChildren<Camera>();
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Chair"))
-        {
-            Debug.Log("Interacted with " + other.name);
-            isInteractable = true;
-        }
+        if (!other.CompareTag("Chair")) return;
         
+        Debug.Log("Interacted with " + other.name);
+        isInteractable = true;
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Chair"))
-        {
-            Debug.Log("Stopped interacting with " + other.name);
-            isInteractable = false;
-        }
+        if (!other.CompareTag("Chair")) return;
+        
+        Debug.Log("Stopped interacting with " + other.name);
+        isInteractable = false;
     }
 
     private void Update()
@@ -43,8 +45,20 @@ public class Interact : MonoBehaviour
             Debug.Log("Sitting down");
             //cam.targetTexture = rt;
             Cursor.lockState = CursorLockMode.None;
+            _playerMovement.enabled = false;
+            _playerCameraScript.enabled = false;
+            character.enabled = false;
+            _rigidbody.isKinematic = true;
+
         }
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _playerMovement.enabled = true;
+            _playerCameraScript.enabled = true;
+            character.enabled = true;
+            _rigidbody.isKinematic = false;
+        }
     }
 }
 
