48778e459a8d705ee554331bcf365a9a4fa88a04
SafwanAlmashnouk <35345@ma-web.nl>
2025-11-11 11:04:55 +0100

    Added new scene for the game over

--- VREscapeRoom/Assets/Scripts/ItemScripts/GameManager.cs
+++ VREscapeRoom/Assets/Scripts/ItemScripts/GameManager.cs
@@ -1,11 +1,15 @@
 using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
+#if UNITY_EDITOR
+    [SerializeField] private SceneAsset sceneAsset;
+#endif
     public TextMeshProUGUI Accuracy, Bias, DataBreach;
     internal float BiasValue, AccuracyValue, DatabreachValue;
 
@@ -14,6 +18,12 @@ public class GameManager : MonoBehaviour
         Accuracy.text = "Accuracy-" + AccuracyValue + "/100";
         Bias.text = "Bias-" + BiasValue + "/100";
         DataBreach.text = "DataBreach-" + DatabreachValue + "/100";
+
+        if(DatabreachValue >= 100 || BiasValue >= 100)
+        {
+            Debug.Log("failed");
+            SceneManager.LoadScene(sceneAsset.name);
+        }
     }
     private void Awake()
     {
